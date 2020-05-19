093ac0b1a43954d6524407f4102b38e83ec3f257
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-05-20 00:12:54 +0200

    textarea for location description

--- resources/views/location/create.blade.php
+++ resources/views/location/create.blade.php
@@ -32,10 +32,11 @@
                                        class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="description" type="text"
-                                           class="form-control @error('description') is-invalid @enderror"
-                                           name="description" value="{{ old('description') }}" required
-                                           autocomplete="description">
+                                    <textarea id="description" type="text"
+                                              class="form-control @error('description') is-invalid @enderror"
+                                              name="description" required autocomplete="description">
+                                        {{ old('description') }}
+                                    </textarea>
 
                                     @error('description')
                                         <span class="invalid-feedback" role="alert">
