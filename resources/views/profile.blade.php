2c19052a5d0386ed943478e13bb1a001f82b49c9
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-05-19 19:15:56 +0200

    make profile locations section optional

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,11 +4,13 @@
     <div class="container py-4">
         <h1>{{ $user->name }} <small class="text-muted">{{  $user->email }}</small></h1>
 
-        <h2>{{ __('Listings by this user') }}</h2>
-        <ul>
-            @foreach($user->locations as $l)
-                <li>{{ $l }}</li>
-            @endforeach
-        </ul>
+        @if($user->locations->isNotEmpty())
+            <h2>{{ __('Listings by this user') }}</h2>
+            <ul>
+                @foreach($user->locations as $l)
+                    <li>{{ $l }}</li>
+                @endforeach
+            </ul>
+        @endif
     </div>
 @endsection
