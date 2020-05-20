7a896170cf053beb2a75e83cf3ce240c3a114431
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-05-20 14:23:05 +0200

    split layout into partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,76 +17,13 @@
 </head>
 <body>
     <div id="app">
-        <header id="page-header" class="page-header">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-                <a href="{{ url('/') }}" class="navbar-brand">
-                    appoint<span class="text-danger">.</span>er
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/locations') }}">{{ __('Locations') }}</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('profile', Auth::user()) }}">
-                                        {{ __('Profile') }}
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('create') }}">
-                                        {{ __('New location') }}
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        @include('partials.header')
 
         <main>
             @yield('content')
         </main>
 
-        <footer id="page-footer" class="container text-center">
-            <div>&#169; appoint.er, <a href="{{ url('imprint') }}">{{ __('imprint.title') }}</a></div>
-        </footer>
+        @include('partials.footer')
     </div>
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer id="page-footer" class="container text-center">
+    <div>&#169; appoint.er, <a href="{{ url('imprint') }}">{{ __('imprint.title') }}</a></div>
+</footer>
