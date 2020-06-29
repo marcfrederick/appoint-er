0a828bdb38cf880e7ba6eca80db94f9c0ff983d5
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-06-29 18:26:25 +0200

    link privacy policy from cookie consent

--- resources/views/partials/cookie-consent.blade.php
+++ resources/views/partials/cookie-consent.blade.php
@@ -1,5 +1,6 @@
 <div id="cookie-consent" style="display: none" class="fixed-bottom jumbotron">
     <h2>Cookie Information and Consent Request</h2>
-    <p>We use cookies to provide our services. Click <a href="https://www.cookiesandyou.com">here</a> to learn more.</p>
+    <p>We use cookies to provide our services. Click <a href="https://www.cookieconsent.com/what-are-cookies/">here</a>
+        to learn more and consult our <a href="{{ route('privacy-policy') }}">privacy policy</a>.</p>
     <div id="cookie-consent-accept" class="btn btn-primary btn-md">Accept</div>
 </div>
