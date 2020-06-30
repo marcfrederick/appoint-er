40337db4466705780464a240746b17246d475dce
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-06-30 15:47:58 +0200

    include a twitter button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,12 @@
     </div>
 
     <div id="page-content" class="container">
+        <div class="float-right">
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-hashtags="appointer"
+               data-dnt="true" data-show-count="false">Tweet</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+
         <p class="lead">{{ __('index.mission') }}</p>
 
         <div class="about-us mt-5">
