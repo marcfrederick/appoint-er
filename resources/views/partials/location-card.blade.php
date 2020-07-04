3c353171a97ae4598e570251b5eec4f79fcac830
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-07-04 16:58:02 +0200

    fix location cards on user profiles

--- resources/views/partials/location-card.blade.php
+++ resources/views/partials/location-card.blade.php
@@ -1,12 +1,9 @@
 <div class="card">
-    <!-- TODO: Add images, probably can just use a default -->
-    <!-- <img class="card-img-top" src="images/wr1.jpeg" alt="an offer image"> -->
     <div class="card-body">
         <a href="{{ route('locations.show', $location->id) }}" class="card-title">{{ $location->title }}</a>
         <p class="card-text">{{ $location->description }}</p>
     </div>
     <div class="card-footer text-center">
-        <a class="btn btn-outline-primary" data-toggle="modal"
-           data-target="#bookingNotImplementedModal">Termin Vereinbaren</a>
+        <a class="btn btn-outline-primary" href="{{ route('locations.show', $location->id) }}">Termin Vereinbaren</a>
     </div>
 </div>
