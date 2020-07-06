0ffbe08932ec3f4e575f13aa274adecd47dede6e
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-07-06 20:38:37 +0200

    fix: tests and type checks

--- app/CategoryLocation.php
+++ app/CategoryLocation.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryLocation extends Model
 {
     protected $table = 'category_location';
+
+    /** @var array<string> */
     protected $fillable = ['category_id', 'location_id'];
 }
