52f9fc2ce9cf2085c5d6936ff0b36e6a82d4e40e
Marc Trölitzsch <ma391tro@htwg-konstanz.de>
2020-07-09 23:19:19 +0200

    add: isCurrentUser böade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -32,5 +34,10 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(self::$defaultStringLength);
         Paginator::defaultView('pagination.default');
         Paginator::defaultSimpleView('pagination.simple');
+
+        \Blade::if('isCurrentUser', function (User $user) {
+            $id = Auth::id();
+            return !is_null($id) && Auth::id() == $user->id;
+        });
     }
 }
